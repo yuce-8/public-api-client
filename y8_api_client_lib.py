8edb7b9576e8925213f37e74a213898adc49b428
YUCE-8 Main Brain <78417048+yuce-8@users.noreply.github.com>
2022-12-16 15:20:22 +0100

    Update y8_api_client_lib.py

--- y8_api_client_lib.py
+++ y8_api_client_lib.py
@@ -29,7 +29,7 @@ class Y8_API_CLIENT:
     def get_latest_forecast(self, symbol='BTCUSD', interval='30min'):
         URL = 'https://storage.googleapis.com/y8-poc/trades/' + ('test' if self.CLIENT_ID is None else self.CLIENT_ID) + '-' + symbol + '-' + interval + '.json'
         SIG = f'get_latest_forecast({symbol}/{interval}) | '
-        printd(SIG, f'requesting @ {URL}')
+        self.debug_out(SIG, f'requesting @ {URL}')
         f_0 = json.loads(requests.get(URL).text)
         run_bug_fixes_for_this_release(f_0)
         return f_0
