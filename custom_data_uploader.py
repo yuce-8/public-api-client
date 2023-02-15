11fa7bb769ee768cf5d96b13a28760c9c6df060e
YUCE-8 Main Brain <78417048+yuce-8@users.noreply.github.com>
2023-02-15 09:22:59 +0100

    Update custom_data_uploader.py

--- custom_data_uploader.py
+++ custom_data_uploader.py
@@ -1,8 +1,8 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+
 import pytz
 import requests
-
 class CustomUploader:
     def __init__(self, symbol) -> None:
         self.symbol = symbol
@@ -18,6 +18,21 @@ class CustomUploader:
             Date_ = Date_.astimezone(DE_TMZ)
         self.buffer.append([Date_.strftime("%Y-%m-%d %H:%M:%S"), Open, High, Low, Close, Volume])
 
+
+    def get_min_max_date_in_db(self):
+        _ = { 'symbol': self.symbol, 'action': 'get_last_date' }
+        r = requests.post('https://europe-west2-yuce-8-v1.cloudfunctions.net/custom_data_loader', json=_)
+        result = r.json()
+        if result['success']:
+            DE_TMZ = pytz.timezone('Europe/Berlin')
+            min_date = DE_TMZ.localize(datetime.datetime.strptime(result['min_date'], '%Y-%m-%d %H:%M:%S')) if not result['min_date'] is None else None
+            max_date = DE_TMZ.localize(datetime.datetime.strptime(result['max_date'], '%Y-%m-%d %H:%M:%S')) if not result['max_date'] is None else None
+            print('min/max date in db = ', min_date, ', ', max_date)
+            return min_date, max_date
+        else:
+            print('error: ', result)
+
+
     def upload(self):
         print(f'upload buffer contains {len(self.buffer)}')
         def divide_chunks(l, n): 
@@ -29,7 +44,7 @@ class CustomUploader:
         counter = 0
         for chunk in chunks:
             counter += 1
-            _ = { 'symbol': self.symbol, 'new_rows' : [] }
+            _ = { 'symbol': self.symbol, 'action': 'upload', 'new_rows' : [] }
             for row in chunk:
                 _['new_rows'].append(row)
             print(f'... uploading chunk {counter} of {len(chunks)}')
@@ -38,7 +53,8 @@ class CustomUploader:
 
 #----------------------------------------------------------------
 
-#custom_uploader = CustomUploader('test_123')
+#custom_uploader = CustomUploader('abc')
+#custom_uploader.get_min_max_date_in_db()
 #custom_uploader.add_to_upload(datetime.datetime.strptime('2022-12-29 20:00:00', '%Y-%m-%d %H:%M:%S'), 10, 12, 8, 11, 123)
 #custom_uploader.add_to_upload(datetime.datetime.strptime('2022-12-29 20:01:00', '%Y-%m-%d %H:%M:%S'), 10, 12, 8, 11, 123)
 #custom_uploader.upload()
