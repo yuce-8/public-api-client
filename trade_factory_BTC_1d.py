8f27b0f26848f77bf9491afd37c1e99a564db21d
YUCE-8 Main Brain <78417048+yuce-8@users.noreply.github.com>
2023-03-03 16:47:54 +0100

    Update trade_factory_BTC_1d.py

--- trade_factory_BTC_1d.py
+++ trade_factory_BTC_1d.py
@@ -661,4 +661,146 @@ class BTC_1d_Wonder:
                     if self.is_discord_available():
                         self.event_listener.discord.post(f'I N V E S T   ==>  direction = {self.direction_of_investment}, now = {last_quote} / {last_date}, trigger price = {self.ready_for_investment_entry_price} before {self.ready_for_investment_maximum_Date_}')
                         
+                        
+#------------------------------ UTILITIES:
+
+def plot_forecast(df, forecast):
+    import matplotlib.pyplot as plt
+    import matplotlib as mpl
+    import datetime
+    import warnings
+    warnings.filterwarnings('ignore')
+    
+    def to_datetime(x):
+        x = str(x).replace('T', ' ')
+        if len(x) > 19:
+          x = x[:19]
+        elif len(x) == 10:
+          x = x + '00:00:00'
+        return datetime.datetime.strptime(x, "%Y-%m-%d %H:%M:%S")
+
+    dpi = 96
+    width = 1400
+    height = 800 # 800
+    mpl.rcParams.update(mpl.rcParamsDefault)
+    fig = plt.figure(figsize=(width/dpi, height/dpi), dpi=dpi)
+    plt.rcParams["axes.grid"] = False
+    plt.style.use("seaborn-bright")
+    plt.rcParams['axes.facecolor'] = 'white'
+    fig.patch.set_facecolor('white')
+    future_dates = []  
+    all_indizes = []
+    fill_colors = ['darkorange', 'mediumspringgreen', 'midnightblue']
+    color_counter = -1  
+    level_1 = forecast['level_1']
+    level_2 = forecast['level_2']
+    model_name = forecast['model_name']
+    training_date = forecast['training_date']
+    last_date_of_forecast = forecast['last_date']
+    last_quote_of_forecast = forecast['last_quote']
+    forecasted_values = forecast['forecast']
+    sl_before, sl_after = forecast['next_support'], forecast['next_resistance'] 
+    is_negative = forecast['is_negative']
+    confidence_extended_1 = forecast['confidence_extended_1']
+    confidence_extended_2 = forecast['confidence_extended_2']
+    forecasted_future_prices_top = forecast['quotes_forecast_top_incl_vola']
+    forecasted_future_prices_bottom = forecast['quotes_forecast_bottom_incl_vola']
+
+    all_quotes_until_forecast = df.loc[df.Date_ <= last_date_of_forecast]
+    all_quotes_after_forecast = df.loc[df.Date_ > last_date_of_forecast]  
+    
+    history = len(all_quotes_after_forecast)
+    all_dates_of_prediction = all_quotes_until_forecast.Date_.values
+    true_date_interval = (to_datetime(all_dates_of_prediction[-1]) - to_datetime(all_dates_of_prediction[-2])).total_seconds() // 60
+    
+    df = df if history == 0 else all_quotes_until_forecast
+    if len(all_quotes_after_forecast) > 0:
+        true_highs = all_quotes_after_forecast.High.head(len(forecasted_future_prices_top)).values
+        true_lows = all_quotes_after_forecast.Low.head(len(forecasted_future_prices_top)).values
+        true_closings = all_quotes_after_forecast.Close.head(len(forecasted_future_prices_top)).values
+        true_dates = [to_datetime(d) for d in all_quotes_after_forecast.Date_.head(len(forecasted_future_prices_top)).values]
+        future_dates = [true_dates[-1] + datetime.timedelta(minutes=true_date_interval * i) for i in range(1, len(forecasted_future_prices_top) + 1)]
+        history = 1
+    plot_history = len(forecasted_values)
+    
+    last_quotes = df['Close'].tail(plot_history).values
+    last_highs = df['High'].tail(plot_history).values
+    last_lows = df['Low'].tail(plot_history).values
+    indizes = df['Close'].tail(plot_history).index
+    indizes_2 = []
+
+    last_quote = df['Close'].tail(1).values[0]
+    indizes_forecast = range(indizes[-1] + 1, indizes[-1]+len(forecasted_values) + 1)
+    
+    bridge_indizes = [indizes[-1], indizes_forecast[0]]
+    bridge_values_1 = [last_quote, forecasted_future_prices_top[0]]
+    bridge_values_2 = [last_quote, forecasted_future_prices_bottom[0]]
+
+    
+    if history > 0:
+        shoting_factor = 0 #if history_size < len(predicted_labels_orig) + 3 else len(predicted_labels_orig) + 5
+        last_quotes_2 = true_closings.tolist()
+        indizes_2 = list(range(list(indizes_forecast)[0], list(indizes_forecast)[0] + len(true_closings)))
+        last_quotes_2.insert(0, last_quote_of_forecast)
+        indizes_2.insert(0, indizes[-1])
+        last_quotes_2 = last_quotes_2[:plot_history+1]
+        indizes_2 = indizes_2[:plot_history+1]
+        plt.plot(indizes_2, last_quotes_2, ':', color='midnightblue', label='Real Close', alpha=0.3)
+        plt.plot(indizes_2[-1 * len(true_highs):], true_highs, '--', color='black', alpha=0.15)
+        plt.plot(indizes_2[-1 * len(true_highs):], true_lows, '--', color='black', alpha=0.15)
+
+
+    plt.axhline(y=last_quotes[-1], color='grey', linestyle='-')
+    plt.plot(indizes, last_quotes, color='black', label='Close')
+    plt.plot(indizes, last_lows, '--', alpha=0.3, color='black', label='High')
+    plt.plot(indizes, last_highs, '--', alpha=0.3, color='black', label='High')
+
+    
+    a = 0.3
+    redyellow = 'yellow' if not is_negative else 'red'
+    plt.fill_between(indizes_forecast, confidence_extended_1, confidence_extended_2, color='green', alpha=0.2)
+    plt.fill_between(indizes_forecast, forecasted_future_prices_top, forecasted_future_prices_bottom, color=redyellow, alpha=0.5)
+
+    plt.plot(indizes[-1], last_quotes[-1], 'bo', label='Forecast', alpha=a, color='navy')
+    plt.plot(indizes_forecast, forecasted_future_prices_top, 'bo', label='Forecast', alpha=a, color='navy')
+    plt.plot(indizes_forecast, forecasted_future_prices_top, 'bo', label='Forecast', alpha=a, color='navy')
+    
+
+    if sl_before != -1 and sl_before > last_quote_of_forecast * 0.9:
+        plt.axhline(sl_before, ls=':', color='orange')
+    if sl_after != 9999999999 and sl_after < last_quote_of_forecast * 1.1:
+        plt.axhline(sl_after, ls=':', color='orange')  
+        
+    trend_1 = forecast['trendline_extended_1'] # extend_trendline(trend_lines[0], iterations=len(indizes_forecast))
+    trend_2 = forecast['trendline_extended_2'] # extend_trendline(
+    plt.plot(indizes_forecast, trend_1, color='blue', alpha=0.5)
+    plt.plot(indizes_forecast, trend_2, color='blue', alpha=0.5)
+      
+      
+
+    default_distance = (indizes_forecast[1] - indizes_forecast[0]) // 2
+    plt.plot(bridge_indizes, bridge_values_1, '--', color='lavender', alpha=a)
+    plt.plot(bridge_indizes, bridge_values_2, '--', color='lavender', alpha=a)
+    all_indizes = [*all_indizes, *indizes, *indizes_2]
+
+
+    
+    plt.title(forecast['symbol'] + ' / ' + forecast['interval'])
+    all_indizes = list(set(all_indizes))[::2]
+    
+    all_indizes = [*indizes, *indizes_2]
+    all_indizes = all_indizes[::2]
+      # printd('** all_indizes: ', all_indizes)
+    
+    last_date_of_forecast = to_datetime(last_date_of_forecast)
+    new_future_dates = [last_date_of_forecast + datetime.timedelta(minutes=true_date_interval * i) for i in range(1, len(forecasted_future_prices_top) + 1, 2)]
+    date_array = [last_date_of_forecast - datetime.timedelta(minutes=true_date_interval * i) for i in range(len(last_quotes) - 2, 0, -2)] + [last_date_of_forecast] + new_future_dates
+
+    if len(all_indizes) < len(date_array):
+        all_indizes = all_indizes[:len(all_indizes)-1]
+        date_array = date_array[:len(all_indizes)]
+    
+    plt.xticks(all_indizes, date_array, rotation=30, ha='right')
+    plt.show()
+
         
